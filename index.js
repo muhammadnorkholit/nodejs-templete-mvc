fc68ef158c22271b82a05e6ce122f3961639cd93
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2023-10-08 13:24:49 +0700

    add env port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set(cors());
 
 app.use("/api", baseRouter);
 
-let port = 5000;
+let port = process.env.PORT || 5000;
 // run server
 app.listen(port, () => {
   console.log("Server running on port " + port);
