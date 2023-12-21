a15e999dc0d0e3044be758ca333445204da1654e
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2023-12-21 22:13:41 +0700

    delete old file

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,4 @@
 const express = require("express");
 const baseRouter = express.Router();
 
-
-
- const users = require("./usersRouter.js");
-baseRouter.use("", users); 
- module.exports = baseRouter;
+module.exports = baseRouter;
